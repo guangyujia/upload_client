82f6cb4df62c1b34a299bbcfbc4eb7955f298098
珠峰架构 <3028213607@qq.com>
2024-08-14 11:05:54 +0800

    新建client

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Upload from './Upload'
+
+const App = function() {
+    return <div className='app'>
+        <Upload/>
+    </div>
+}
+
+export default App
---
+++ client/src/Upload.tsx
@@ -0,0 +1,20 @@
+import React, { ChangeEvent } from 'react'
+import {Row, Col, Input} from 'antd'
+
+const Upload = function() {
+    const handleChange = (event:ChangeEvent<HTMLInputElement>) => {
+
+    }
+  return (<div>
+    <Row>
+        <Col span={12}>
+            <Input type='file' style={{width: 300}} onChange={handleChange}/>
+        </Col>
+        <Col span={12}>
+
+        </Col>
+    </Row>
+  </div>)
+}
+
+export default Upload;
